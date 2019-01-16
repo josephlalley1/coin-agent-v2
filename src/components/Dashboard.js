a54a6bd58d62d978046ca2c74ac256372ab2bce5
Joseph Lalley <josephyzyr@gmail.com>
2019-01-16 14:35:15 +0000

    changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,58 +1,32 @@
 import React from 'react';
 import axios from 'axios';
-import ChartLine from './Line.js';
+import { Link } from 'react-router-dom';
+import { decodeToken } from '../lib/auth';
 
-class Home extends React.Component {
+class TradeIndex extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      chartData: {
-        labels: [],
-        datasets: {
-
-        }
-      }
-    };
+    this.state = {};
   }
 
   componentDidMount() {
-    axios.get('https://api.coinranking.com/v1/public/coins')
-      .then((result) => {
-        result.data.data.coins.length = 20;
-        this.setState({ externalData: result.data.data.coins });
-        console.log(this.state.externalData);
-      });
-
-  }
-
-  numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    axios.all([
+      axios.get('/api/trades'),
+      axios.get('https://api.coinranking.com/v1/public/coins')
+    ])
+      .then(axios.spread((localTrades, externalCoinData) => {
+        this.setState({ trades: localTrades.data });
+        this.setState({ externalData: externalCoinData.data.data.coins });
+        console.log(this.state);
+        const a = this.state.externalData;
+        console.log(a);
+      }));
   }
 
   render() {
-    const { externalData } = this.state;
-    if (!externalData) return null;
-
-    const data = externalData.reduce((lineChartData, coin) => {
-      console.log(lineChartData);
-      lineChartData[coin.name] = coin.history.map((price) => [
-        parseFloat(price)
-      ]);
-      return lineChartData;
-    }, {});
-    console.log(data);
-
     return (
       <div>
         <h1>DASHBOARD</h1>
-        {this.state.externalData && this.state.externalData.map(
-          (coin, i) =>
-            <div key={i} className="chart-box" style={{display: 'inline-block'}}>
-              <h1 style={{textAlign: 'center'}}>{coin.name}</h1>
-              <ChartLine key={i} data={data[coin.name]}/>
-              {console.log('this is the data for', coin.name, ':', data[coin.name])}
-            </div>)}
-
         <div className="portfolio-container">
           <div className="pa4">
             <div className="overflow-auto">
@@ -61,22 +35,26 @@ class Home extends React.Component {
                   <tr>
                     <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Coin</th>
                     <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Coin Name</th>
-                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Price</th>
+                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Amount</th>
                     <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Symbol</th>
-                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Market Cap</th>
-                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Website</th>
+
+                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Value</th>
+                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">ID</th>
+                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white"></th>
                   </tr>
                 </thead>
                 <tbody className="lh-copy">
-                  {this.state.externalData && this.state.externalData.map(
-                    (coin, i) =>
+                  {this.state.trades && this.state.trades.filter(trade => trade.transactionAddedBy === decodeToken().sub).map(
+                    (trade, i) =>
                       <tr key={i}>
-                        {this.state.externalData && <td className="pv3 pr3 bb b--light-gray"><img src={coin.iconUrl} width="30px"/></td>}
-                        <td className="pv3 pr3 bb b--light-gray">{coin.name}</td>
-                        <td className="pv3 pr3 bb b--light-gray">${parseFloat(coin.price).toFixed(2)}</td>
-                        <td className="pv3 pr3 bb b--light-gray">{coin.symbol}</td>
-                        <td className="pv3 pr3 bb b--light-gray">${this.numberWithCommas(coin.marketCap)}</td>
-                        <td className="pv3 pr3 bb b--light-gray"><a href={coin.websiteUrl} className="remove-a-styling">{coin.websiteUrl}</a></td>
+                        {this.state.externalData && <td className="pv3 pr3 bb b--light-gray"><img src={this.state.externalData.filter(coin => coin.symbol === trade.symbol)[0].iconUrl} width="30px"/></td>}
+                        <td className="pv3 pr3 bb b--light-gray">{trade.coinName}</td>
+                        <td className="pv3 pr3 bb b--light-gray">{trade.transactionTotal}</td>
+                        <td className="pv3 pr3 bb b--light-gray">{trade.symbol}</td>
+
+                        {this.state.externalData && <td className="pv3 pr3 bb b--light-gray">${(trade.transactionTotal * parseFloat(this.state.externalData.filter(coin => coin.symbol === trade.symbol)[0].price)).toFixed(2) }</td> }
+                        <td className="pv3 pr3 bb b--light-gray">{trade._id}</td>
+                        <td className="pv3 pr3 bb b--light-gray tr"><Link to={`/trades/${trade._id}`}  key={i} className="remove-a-styling">View Trade</Link></td>
                       </tr>
                   )}
                 </tbody>
@@ -84,9 +62,9 @@ class Home extends React.Component {
             </div>
           </div>
         </div>
+
       </div>
     );
   }
 }
-
-export default Home;
+export default TradeIndex;
