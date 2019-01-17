8aa4936422aa1309d2ec4601fd907686c9e83396
Joseph Lalley <josephyzyr@gmail.com>
2019-01-17 21:10:51 +0000

    moving data collection to backend

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -4,10 +4,25 @@ function totalAssetsValue() {
   rp({
     method: 'GET',
     url: 'https://api.coinranking.com/v1/public/coins'
-  }).then(result => {
-    const results = JSON.parse(result)
-    console.log(results.data)
+  }).then(externalResult => {
+    const externalData = JSON.parse(externalResult)
     console.log('new request')
+    rp({
+      method: 'GET',
+      url: 'http://localhost:4000/api/trades'
+    }).then(expressResult => {
+      const trades = JSON.parse(expressResult)
+      const totalPortfolioValue = []
+      const reducer = (accumulator, currentValue) => accumulator + currentValue
+      console.log(trades)
+      console.log(externalData.data.coins)
+      trades.filter(trade => trade.transactionAddedBy === '5be9ab34206c80cc7c53dd09').map(
+        (trade => {
+          totalPortfolioValue.push(trade.transactionTotal * parseFloat(externalData.data.coins.filter(coin => coin.symbol === trade.symbol)[0].price))
+        })
+      )
+      console.log(totalPortfolioValue.reduce(reducer))
+    })
   })
 }
 
