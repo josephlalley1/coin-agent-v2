93153341a0a70af67943a53c1e3d7980a6d90d0d
Joseph Lalley <josephyzyr@gmail.com>
2019-01-21 21:26:31 +0000

    trades work

--- src/components/trades/Index.js
+++ src/components/trades/Index.js
@@ -25,44 +25,39 @@ class TradeIndex extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="portfolio-container">
-          <div className="pa4">
-            <div className="overflow-auto">
-              <table className="f6 w-100 mw8 center" cellSpacing="0">
-                <thead>
-                  <tr>
-                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Coin</th>
-                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Coin Name</th>
-                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Amount</th>
-                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Symbol</th>
-
-                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">Value</th>
-                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white">ID</th>
-                    <th className="fw6 bb b--light-gray tl pb3 pr3 bg-white"></th>
+      <div className="trades-container">
+        <h2 className="heading">My Trades</h2>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th className="coin-table">Coin</th>
+                <th className="coinname-table">Coin Name</th>
+                <th className="symbol-table">Symbol</th>
+                <th className="qty-table">Qty</th>
+                <th className="price-table">Price</th>
+                <th className="qty-table">Value</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.trades && this.state.trades.filter(trade => trade.transactionAddedBy === decodeToken().sub).map(
+                (trade, i) =>
+                  <tr key={i}>
+                    {this.state.externalData && <td><img src={this.state.externalData.filter(coin => coin.symbol === trade.symbol)[0].iconUrl} width="30px"/></td>}
+                    <td>{trade.coinName}</td>
+                    <td>{trade.symbol}</td>
+                    <td>{trade.transactionTotal}</td>
+                    { this.state.externalData && <td>{this.state.externalData.filter(coin => coin.symbol === trade.symbol)[0].price}</td>}
+                    { this.state.externalData && <td>${(trade.transactionTotal * parseFloat(this.state.externalData.filter(coin => coin.symbol === trade.symbol)[0].price)).toFixed(2) }</td> }
+                    <td><Link to={`/trades/${trade._id}`}  key={i}>View Trade</Link></td>
                   </tr>
-                </thead>
-                <tbody className="lh-copy">
-                  {this.state.trades && this.state.trades.filter(trade => trade.transactionAddedBy === decodeToken().sub).map(
-                    (trade, i) =>
-                      <tr key={i}>
-                        {this.state.externalData && <td className="pv3 pr3 bb b--light-gray"><img src={this.state.externalData.filter(coin => coin.symbol === trade.symbol)[0].iconUrl} width="30px"/></td>}
-                        <td className="pv3 pr3 bb b--light-gray">{trade.coinName}</td>
-                        <td className="pv3 pr3 bb b--light-gray">{trade.transactionTotal}</td>
-                        <td className="pv3 pr3 bb b--light-gray">{trade.symbol}</td>
-
-                        {this.state.externalData && <td className="pv3 pr3 bb b--light-gray">${(trade.transactionTotal * parseFloat(this.state.externalData.filter(coin => coin.symbol === trade.symbol)[0].price)).toFixed(2) }</td> }
-                        <td className="pv3 pr3 bb b--light-gray">{trade._id}</td>
-                        <td className="pv3 pr3 bb b--light-gray tr"><Link to={`/trades/${trade._id}`}  key={i} className="remove-a-styling">View Trade</Link></td>
-                      </tr>
-                  )}
-                </tbody>
-              </table>
-            </div>
-          </div>
+              )}
+            </tbody>
+          </table>
         </div>
-
       </div>
+
     );
   }
 }
